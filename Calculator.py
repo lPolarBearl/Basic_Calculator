b89508b8e85f09091be19d3c5803a9bb41ec0edc
Polar Bear <69587185+PolarBearGithub-sudo@users.noreply.github.com>
2023-02-24 21:55:13 +0100

    Maximum code reduction
    I decided to reduce the code to an obscene size.

--- Calculator.py
+++ Calculator.py
@@ -1,35 +1,23 @@
+import math
 Number_1 = int(input("What one number? "))
 Number_2 = int(input("What two number? "))
-import math
-# Functions
-def Plus():
-   if WhatNeed == 1:
-    print(Number_1 + Number_2)
-def Minus():
-    print(Number_1 - Number_2)
-def Dellint():
-    print(Number_1 / Number_2)
-def X():
-    print(Number_1 * Number_2)
-def Exp(a,b):
+def Exp(a,b): #exponent
     out = a
     for _ in range(b-1):
         out *=a
     return out
-def SQRT(a):
-    return math.sqrt(a)
 WhatNeed = int(input("1. + 2. - 3. X 4. / 5. Exponentiation (Number_2: Degree). 6. Square root (Number_1);"))
 if WhatNeed == 1:
-    Plus()
+    print(Number_1 + Number_2)
 elif WhatNeed == 2:
-    Minus()
+    print(Number_1 - Number_2)
 elif WhatNeed == 3:
-    X()
+    print(Number_1 * Number_2)
 elif WhatNeed == 4:
-    Dellint()
+    print(Number_1 / Number_2)
 elif WhatNeed == 5:
     print(Exp(Number_1,Number_2))
 elif WhatNeed == 6:
-    print(SQRT(Number_1))
+    print(math.sqrt(Number_1))
 else:
     print("Unknown operation")
