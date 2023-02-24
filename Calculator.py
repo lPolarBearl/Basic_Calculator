6f961ef3a5aa44e8747352bb5b31df8e7204d3ae
Polar Bear <69587185+PolarBearGithub-sudo@users.noreply.github.com>
2023-02-24 20:51:31 +0100

    Added new math operation
    Added Square root and Exponentiation.

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
-# Basic calculator
 Number_1 = int(input("What one number? "))
 Number_2 = int(input("What two number? "))
+import math
 # Functions
 def Plus():
    if WhatNeed == 1:
@@ -11,15 +11,25 @@ def Dellint():
     print(Number_1 / Number_2)
 def X():
     print(Number_1 * Number_2)
-WhatNeed = int(input("1. + 2. - 3. X 4. / ;" ))
+def Exp(a,b):
+    out = a
+    for _ in range(b-1):
+        out *=a
+    return out
+def SQRT(a):
+    return math.sqrt(a)
+WhatNeed = int(input("1. + 2. - 3. X 4. / 5. Exponentiation (Number_2: Degree). 6. Square root (Number_1);"))
 if WhatNeed == 1:
     Plus()
-    
 elif WhatNeed == 2:
     Minus()
 elif WhatNeed == 3:
     X()
 elif WhatNeed == 4:
     Dellint()
+elif WhatNeed == 5:
+    print(Exp(Number_1,Number_2))
+elif WhatNeed == 6:
+    print(SQRT(Number_1))
 else:
-    print("Unknown opiration.")
+    print("Unknown operation")
