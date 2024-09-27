b9c8106485f9d8a3761ca90c71b1d0374dd2d8a9
Polar Bear <69587185+lPolarBearl@users.noreply.github.com>
2024-09-27 22:03:33 +0000

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,21 +3,13 @@ Number_1 = int(input("What one number? "))
 Number_2 = int(input("What two number? "))
 def Exp(a,b): #exponent
     out = a
-    for _ in range(b-1):
-        out *=a
+    for _ in range(b-1):out *=a
     return out
 WhatNeed = int(input("1. + 2. - 3. X 4. / 5. Exponentiation (Number_2: Degree). 6. Square root (Number_1);"))
-if WhatNeed == 1:
-    print(Number_1 + Number_2)
-elif WhatNeed == 2:
-    print(Number_1 - Number_2)
-elif WhatNeed == 3:
-    print(Number_1 * Number_2)
-elif WhatNeed == 4:
-    print(Number_1 / Number_2)
-elif WhatNeed == 5:
-    print(Exp(Number_1,Number_2))
-elif WhatNeed == 6:
-    print(math.sqrt(Number_1))
-else:
-    print("Unknown operation")
+if WhatNeed == 1:print(Number_1 + Number_2)
+elif WhatNeed == 2:print(Number_1 - Number_2)
+elif WhatNeed == 3:print(Number_1 * Number_2)
+elif WhatNeed == 4:print(Number_1 / Number_2)
+elif WhatNeed == 5:print(Exp(Number_1,Number_2))
+elif WhatNeed == 6:print(math.sqrt(Number_1))
+else:print("Unknown operation")
